a2ef751ca2b2f360a11e4c80d8ebc622d6300837
hardik lamboria <hardiklamboria1@gmail.com>
2026-07-14 16:49:49 +0530

    Time: 0 ms (100%), Space: 23 MB (52.07%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+
+        vector<int> count(n, 1);
+
+        // Left to Right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                count[i] = count[i - 1] + 1;
+            }
+        }
+
+        // Right to Left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                count[i] = max(count[i], count[i + 1] + 1);
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += count[i];
+        }
+
+        return sum;
+    }
+};
